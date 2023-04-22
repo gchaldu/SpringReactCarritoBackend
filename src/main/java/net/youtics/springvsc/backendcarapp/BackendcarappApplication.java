5c31e11caa4c02a680591d57ef8981c600cd5c92
criaturitavalu <youtics@gmail.com>
2023-04-22 12:27:01 -0300

    Version 1 con mapeo basico de la clase product

---
+++ src/main/java/net/youtics/springvsc/backendcarapp/BackendcarappApplication.java
@@ -0,0 +1,13 @@
+package net.youtics.springvsc.backendcarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendcarappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendcarappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/youtics/springvsc/backendcarapp/BackendcarappApplicationTests.java
@@ -0,0 +1,13 @@
+package net.youtics.springvsc.backendcarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendcarappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
